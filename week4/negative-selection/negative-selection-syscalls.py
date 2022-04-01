3029046de086c0fd4d5e4f1b79cb4714020aaf8d
yanavdsande <yana.sande@gmail.com>
2022-04-01 20:08:04 +0200

    without bug

--- week4/negative-selection/negative-selection-syscalls.py
+++ week4/negative-selection/negative-selection-syscalls.py
@@ -4,6 +4,7 @@ import re
 import pandas as pd
 from sklearn import metrics
 import matplotlib.pyplot as plt
+import numpy as np
 
 #%%
 def chunk(inputfile, n, outputfile):
@@ -35,8 +36,7 @@ def chunk(inputfile, n, outputfile):
     f.close()
 
     return chunks
-#%%
-#function call 
+
 
 #%%
 def auc_calculation(scorefile, labels, chunks, rpar, npar):
@@ -70,68 +70,70 @@ def auc_calculation(scorefile, labels, chunks, rpar, npar):
     labels = labels[0].values
     processed_data['labels'] = labels
 
-    #create the unique values list
-    co_val = processed_data['scores'].unique()
-
-    #calculate sensitivity: 
-    sensitivity = []
-    for i in range(0,len(co_val)):        #iterate over cut off value list
-        df_mask=processed_data['scores']>co_val[i]  #create boolean mask
-        df_pos = processed_data[df_mask]            #select values lower than cut off values
-        sensitivity.append(len(df_pos.loc[df_pos['labels']==1])) #append the # of values that is higher than cutoff value and is anomaly
-
-    sensitivity = [value/len(processed_data.loc[processed_data['labels']==1]) for value in sensitivity] #calculate the sensitivity percentage
-
-    #calculate specificity 
-    specificity = []
-
-    for i in range(0,len(co_val)):       #iterate over cut off value list
-        df_mask=processed_data['scores']<co_val[i] #create boolean mask
-        df_ts = processed_data[df_mask]            #select values lower than cut off values
-        specificity.append(len(df_ts.loc[df_ts['labels']==1])) #append the # of values that is lower than cutoff value and is normal
-
-    specificity = [value/len(processed_data.loc[processed_data['labels']==1]) for value in specificity] #calculate specificity
-
-    #calculate false positive rate: 
-    fpr = [1 - value for value in specificity] #calculate false positive rate
-
-    #convert to numpy array
-    fpr = np.array(fpr)
-    sensitivity = np.array(sensitivity)
-
-    #sort the fpr array and the sensitivity array for plotting purposes
-    ix = fpr.argsort()
-    fpr_sorted = fpr[ix]
-    sen_sorted = sensitivity[ix]
-
-    #calculate the auc and print
-    auc = metrics.auc(fpr_sorted, sen_sorted)
-    result = f"AUC value is {auc}"
-    print(result)
-
-    #plot the roc curve
-    plt.figure()
-    plt.plot(fpr_sorted, sen_sorted)
-    plt.xlabel('fpr')
-    plt.ylabel('sensitivity')
-    plt.title(f'ROC curve with -r {rpar} and -n {npar}')
-    plt.figtext(0.5, -0.1, text)
-    plt.show()
-
-
-
-
+    auc = metrics.roc_auc_score(y_true=processed_data["labels"], y_score=processed_data["scores"])
+    print(auc)
 
 #%%
 #generate data: 
-chunks = chunk("C:/Users/yanas/OneDrive/Masters/Untitled Folder 1/NatCom2022/week4/negative-selection/syscalls/snd-cert/snd-cert.2.test", 7, "snd-cert-2-pp.test")
+chunks = chunk("C:/Users/yanas/OneDrive/Masters/Untitled Folder 1/NatCom2022/week4/negative-selection/syscalls/snd-unm/snd-unm.3.test", 7, "snd-unm-3-7.test")
 #%%
 #generate auc value 
-auc_calculation("C:/Users/yanas/OneDrive/Masters/Untitled Folder 1/NatCom2022/week4/negative-selection/snd-cert-2-scores-3.csv", 
-"C:/Users/yanas/OneDrive/Masters/Untitled Folder 1/NatCom2022/week4/negative-selection/syscalls/snd-cert/snd-cert.2.labels", 
+auc_calculation("C:/Users/yanas/OneDrive/Masters/Untitled Folder 1/NatCom2022/week4/negative-selection/scores-snd-unm-3-7-3.csv", 
+"C:/Users/yanas/OneDrive/Masters/Untitled Folder 1/NatCom2022/week4/negative-selection/syscalls/snd-unm/snd-unm.3.labels", 
 chunks, 
-5, 
+2, 
 7)
 # r = 3 0.48 
 
 # %%
+
+#The following code is an own implementation of AUC value because we didn't believe the values in the first place
+
+# #create the unique values list
+    # co_val = processed_data['scores'].unique()
+
+    # #calculate sensitivity: 
+    # sensitivity = []
+    # for i in range(0,len(co_val)):        #iterate over cut off value list
+    #     df_mask=processed_data['scores']>co_val[i]  #create boolean mask
+    #     df_pos = processed_data[df_mask]            #select values lower than cut off values
+    #     sensitivity.append(len(df_pos.loc[df_pos['labels']==1])) #append the # of values that is higher than cutoff value and is anomaly
+    # print(df_pos)
+    # sensitivity = [value/len(processed_data.loc[processed_data['labels']==1]) for value in sensitivity] #calculate the sensitivity percentage
+
+    # #calculate specificity 
+    # specificity = []
+
+    # for i in range(0,len(co_val)):       #iterate over cut off value list
+    #     df_mask=processed_data['scores']<co_val[i] #create boolean mask
+    #     df_ts = processed_data[df_mask]            #select values lower than cut off values
+    #     specificity.append(len(df_ts.loc[df_ts['labels']==0])) #append the # of values that is lower than cutoff value and is normal
+    #     print(df_ts)
+    # specificity = [value/len(processed_data.loc[processed_data['labels']==1]) for value in specificity] #calculate specificity
+
+    # #calculate false positive rate: 
+    # fpr = [1 - value for value in specificity] #calculate false positive rate
+
+    # #convert to numpy array
+    # fpr = np.array(fpr)
+    # sensitivity = np.array(sensitivity)
+
+    # #sort the fpr array and the sensitivity array for plotting purposes
+    # ix = fpr.argsort()
+    # fpr_sorted = fpr[ix]
+    # sen_sorted = sensitivity[ix]
+
+    # #calculate the auc and print
+    # auc = metrics.auc(fpr_sorted, sen_sorted)
+    # result = f"AUC value is {auc}"
+    # print(result)
+
+    # #plot the roc curve
+    # plt.figure()
+    # plt.plot(fpr_sorted, sen_sorted)
+    # plt.xlabel('fpr')
+    # plt.ylabel('sensitivity')
+    # plt.title(f'ROC curve with -r {rpar} and -n {npar}')
+    # plt.figtext(0.5, -0.1, result)
+    # plt.show()
+
